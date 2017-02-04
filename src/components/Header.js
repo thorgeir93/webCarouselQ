6cab04656b5b963d581bb1aa0775548a39dd401b
thorgeir93 <thorgeirsigurd@gmail.com>
2017-02-04 01:34:03 +0000

    Minor setup for Header and Search

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import { Link } from 'react-router';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+      </div>
+    );
+  }
+}
+		//<img src="logo.png" alt="Logo App" className="image"/>
