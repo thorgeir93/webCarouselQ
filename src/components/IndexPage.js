cf99b7dd4ddfab7134686120ad6bdffee48e0ced
thorgeir93 <thorgeirsigurd@gmail.com>
2017-03-12 17:32:18 +0000

    Minor changes.

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -2,12 +2,15 @@
 
 import React from 'react';
 import ListPreview from './ListPreview';
+import Header from './Header';
+import Search from './Search';
 import songs from '../data/mockSongs'
 
 export default class IndexPage extends React.Component {
   render() {
     return (
       <div className="home">
+        <Header></Header>
         <div className="athletes-selector">
             <h1>Title</h1>
             {songs.map(songData => <ListPreview key={songData.name} {...songData} />)}
